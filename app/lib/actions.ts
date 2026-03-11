1df98c01d77676539d95a33541f2e0aeefaf777f
code-corax <gracefalconette@gmail.com>
2026-03-11 01:28:21 -0400

    chapter 11-12 changes

---
+++ app/lib/actions.ts
@@ -0,0 +1,85 @@
+'use server';
+
+import { z } from 'zod';
+import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch(error) {
+    console.log(error);
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+
+  //only available if the try is successful, cause no finally
+  revalidatePath('/dashboard/invoices'); //revalidate cache because an insert has occurred, changing the data state
+  redirect('/dashboard/invoices'); //redirect now that creation has finished, like closing a modal but instead it's pathing
+
+  // Test it out:
+  //Object.fromEntries(entries); console.log this to make extracting from objects easier
+//   console.log(rawFormData);
+//   console.log(typeof rawFormData.amount);
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch(error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
+  }
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+
+  throw new Error('Failed to Delete Invoice');
+
+  
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices'); //will trigger new server request and re-render the table
+}
